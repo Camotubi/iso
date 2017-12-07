ad0f454203667cf5dc1449bdecb0737c6cf0e0cc
Roy Henriquez <roy.henriquez@gmail.com>
2017-12-07 01:25:34 -0500

    Parentesis en las formulas

--- database/seeds/MetricsTableSeeder.php
+++ database/seeds/MetricsTableSeeder.php
@@ -14,19 +14,19 @@ class MetricsTableSeeder extends Seeder
     {
       $metric = new Metric;
       $metric->name = 'Functional adequacy';
-      $metric->formula = '1-A/B';
+      $metric->formula = '1-(A/B)';
       $metric->subcharacteristic()->associate(1);
       $metric->save();
 
       $metric = new Metric;
       $metric->name = 'Functional implementation coverage';
-      $metric->formula = '1-A/B';
+      $metric->formula = '1-(A/B)';
       $metric->subcharacteristic()->associate(1);
       $metric->save();
 
       $metric = new Metric;
       $metric->name = 'Functional implementation completeness';
-      $metric->formula = '1-A/B';
+      $metric->formula = '1-(A/B)';
       $metric->subcharacteristic()->associate(1);
       $metric->save();
     }
