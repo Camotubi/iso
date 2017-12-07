aed2fefc39f2630c93334e84f2ae5aec71c189d5
Camotubi <camotubi@gmail.com>
2017-12-06 19:46:12 -0500

    Add base layout, css, js and component

---
+++ resources/views/components/navbar1.blade.php
@@ -0,0 +1,34 @@
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+		<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+		<a class="navbar-brand" href="#">Sinclair</a>
+		<ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+			<li class="nav-item active">
+				<a class="nav-link" href="/dashboard">Dashboard</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/">Pagina Principal</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="/stats">Estadisticas</a>
+			</li>
+		</ul>
+		<form class="form-inline my-2 my-lg-0">
+			<input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+			<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+		</form>
+		<div >
+			@isset($user)
+			<div class="dropdown">
+				<button class ="btn btn-outline-info my-2 my-sm-0 dropdown-toggle nav-item" type ="button" id="userDropDownMenu" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true">
+				{{$user->name}}
+			</button>
+			<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+    <a class="dropdown-item" href="/logout">Cerrar Sesion</a>
+  </div>
+		</div>
+			@endisset
+		</div>
+	</div>
+
---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <title>Sinclair Backroom - @yield('title')</title>
+        <link rel="stylesheet" href="/css/app.css">
+        <link rel="stylesheet" href="/css/dashboard.css">
+        <link rel="stylesheet" href="/css/custom.css">
+        <!--        <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css"> -->
+    </head>
+    <body>
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            @include('components.navbar1')
+        </nav>
+    @show
+
+    <div id="app" class="container-fluid">
+        <div class="row">
+            <nav class="col-sm-3 col-md-2 d-none d-sm-block navbar navbar-light sidebar flex-column sidebar">
+                @include('components.sidebar1')
+            </nav>
+            <main style="padding:20px;"class="col-sm-9 col-sm-offset-3 col-md-10 col-md-offset-2 main">
+                @if(session('error'))
+                    <div class="alert alert-danger">
+                        {{session('error')}}
+                    </div>
+                @endif
+                @if(session('success'))
+                    <div class="alert alert-success">
+                        {{session('success')}}
+                    </div>
+                @endif
+                @yield('content')
+            </main>
+        </div>
+    </div>
+    <footer class="footer">
+        <img src="/img/utp_logo.png" alt="" height="50px" id="footer_img">
+        <h6 class="small" id="footer_legal">COPYRIGHT © 2017 UTP TODOS LOS DERECHOS RESERVADOS</h6>
+    </footer>
+    <script type="text/javascript" src="/js/app.js"></script>
+</body>
+</html>
