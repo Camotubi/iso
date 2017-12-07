6d724a61f3ab4bf35025b324eff0addd06dd3971
Roy Henriquez <roy.henriquez@gmail.com>
2017-12-07 08:33:32 -0500

    Mods de create form

--- resources/views/project/deliverable/metricDeliverable/evaluation/create.blade.php
+++ resources/views/project/deliverable/metricDeliverable/evaluation/create.blade.php
@@ -11,7 +11,9 @@
             <p>Formula de la Métrica: {{$metric->formula}}</p>
             <br>
             @for($i=0; $i<10;$i++)
+              <label for="">Variable</label>
                 <input type="text" name="variable[]" class="form-control">
+              <label for="">Valor</label>
                 <input type="number" name="value[]" step="any" class="form-control">
             @endfor
             <input class= "btn btn-primary" type = "submit" value = "Registrar">
