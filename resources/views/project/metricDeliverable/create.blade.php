78e676c7552b23e56b2f9a5c236b2c0d9b645797
tarowookjin <tarowj05@gmail.com>
2017-12-07 03:26:20 -0500

    Formulario para registro de metricDeliverable

--- resources/views/project/metricDeliverable/create.blade.php
+++ resources/views/project/metricDeliverable/create.blade.php
@@ -1,2 +1,22 @@
 
 @extends('layouts.app')
+
+@section('content')
+    <h1> Registro de Metrica</h1>
+    <form method="POST" action="/projects/{{$project->id}}/metricDeliverable">
+        <div class = "container">
+            {{ csrf_field() }}
+            <div class="form-group">
+            <label>Metrica:</label>
+            <select class = "form-control" name = "metric" required>
+              <option></option>
+            </select>
+            <label>Entregable:</label>
+            <select class = "form-control" name = "deliverable" required>
+              <option></option>
+            </select>
+            </div>
+            <input class= "btn btn-primary" type = "submit" value = "Registrar">
+        </div>
+    </form>
+@endsection
