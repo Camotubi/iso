d99624a5c360b6ba330f28e407fb7a2359e73026
Camotubi <camotubi@gmail.com>
2017-12-09 21:35:44 -0500

    add add-metric-form vue component
    Partiall adtition

---
+++ resources/assets/js/components/AddMetric.vue
@@ -0,0 +1,46 @@
+<template>
+    <form>
+        <select>
+            <option v-for = "qA in qualityAttributes" :value="qA.id">
+                {{qA.name}}
+            </option>
+        </select>
+    </form>
+
+
+
+</template>
+<style>
+
+</style>
+
+<script>
+
+export default  {
+    data: function() {
+        return {
+            qualityAttributes:{},
+            characteristics:{},
+            subcharacteristics:{},
+            metrics:{},
+        }
+    },
+    created() {
+		// Fetch initial results
+		this.getResults();
+	},
+    methods: {
+		// Our method to GET results from a Laravel endpoint
+		getResults(page) {
+			var self = this
+			// Using vue-resource as an example
+			axios.get('/api/qualityAttributes/').then(
+				function(response){
+					self.qualityAttributes = response.data;
+				});
+		}
+	}
+
+}
+
+</script>
