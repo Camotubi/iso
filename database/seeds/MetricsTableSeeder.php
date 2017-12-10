01f6f9aea6b6ecb80750f725265ffa7ca507c87b
Roderik Acevedo <rode31x@gmail.com>
2017-12-10 16:48:10 -0500

    Portability Metrics

--- database/seeds/MetricsTableSeeder.php
+++ database/seeds/MetricsTableSeeder.php
@@ -608,5 +608,64 @@ class MetricsTableSeeder extends Seeder
       $metric->formula = '1-(A/B)';
       $metric->subcharacteristic()->associate(11);
       $metric->save();
+
+      #---------------------Portability Metrics Associate(12)-----------#
+      //Adaptabilouty Metrics
+      $metric = new Metric;
+      $metric->name = 'Adaptability of Data Structures';
+      $metric->description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+       culpa qui officia deserunt mollit anim id est laborum.';
+      $metric->formula = 'A/B';
+      $metric->subcharacteristic()->associate(12);
+      $metric->save();
+
+      //Installability Metrics
+      $metric = new Metric;
+      $metric->name = 'Ease of Installation';
+      $metric->description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+       culpa qui officia deserunt mollit anim id est laborum.';
+      $metric->formula = 'A/B';
+      $metric->subcharacteristic()->associate(12);
+      $metric->save();
+
+      //Replaceability Metrics
+      $metric = new Metric;
+      $metric->name = 'Continued Use of Data';
+      $metric->description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+       culpa qui officia deserunt mollit anim id est laborum.';
+      $metric->formula = 'A/B';
+      $metric->subcharacteristic()->associate(12);
+      $metric->save();
+
+      //Portability Compliance Metrics
+      $metric = new Metric;
+      $metric->name = 'Portability Compliance';
+      $metric->description = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+       sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+       Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris
+       nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+       reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+       pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+       culpa qui officia deserunt mollit anim id est laborum.';
+      $metric->formula = '1-(A/B)';
+      $metric->subcharacteristic()->associate(12);
+      $metric->save();
+
+
     }
 }
