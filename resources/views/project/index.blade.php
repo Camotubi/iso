74153d1a081735d811bb01b49682c59a7e62d619
Camotubi <camotubi@gmail.com>
2017-12-10 12:46:18 -0500

    fix create project url not using absolute url

--- resources/views/project/index.blade.php
+++ resources/views/project/index.blade.php
@@ -17,5 +17,5 @@
 			</tr>
 		@endforeach
 		</table>
-	<a href="projects/create" class="btn btn-primary button">Crear Proyecto</a>
+	<a href="/projects/create" class="btn btn-primary button">Crear Proyecto</a>
 @endsection
